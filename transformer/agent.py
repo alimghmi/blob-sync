886cb2d0cf048432e7129d647d6e70766b20e173
Ali Moghimi <mghmiali@gmail.com>
2024-01-29 13:21:20 +0000

    feat: add debug logging and breaking down logic in parse_files

--- transformer/agent.py
+++ transformer/agent.py
@@ -24,30 +24,39 @@ class Agent:
     def parse_files(self) -> None:
         try:
             for path in self.files:
-                folder, file = path.split("/")
-                isin = file.split("_")[-1].split(".")[0]
-
+                isin, folder = self.extract_isin_and_folder(path)
                 if isin not in self.files_per_isin:
                     self.files_per_isin[isin] = {}
 
                 self.files_per_isin[isin][folder] = path
+            logger.debug("File parsing completed.")
         except Exception as e:
             logger.error(f"Failed to parse the files list. Error: {e}")
             raise
 
     def init_df(self) -> None:
         try:
+            logger.debug("Initializing DataFrame from parsed data.")
             self.dataframe = pd.DataFrame(self.files_per_isin).transpose()
             self.dataframe.reset_index(inplace=True)
             self.dataframe.rename(columns={"index": "ISIN"}, inplace=True)
             self.dataframe.replace({np.nan: None}, inplace=True)
+            logger.debug("DataFrame initialized successfully.")
         except Exception as e:
             logger.error(f"Failed to init dataframe. Error: {e}")
             raise
 
     def add_timestamp(self) -> None:
         try:
+            logger.debug("Adding timestamp to DataFrame.")
             self.dataframe["timestamp_created_utc"] = datetime.datetime.utcnow()
+            logger.debug("Timestamp added successfully.")
         except Exception as e:
             logger.error(f"Failed to add timestamps. Error: {e}")
             raise
+
+    @staticmethod
+    def extract_isin_and_folder(path):
+        folder, file = path.split("/")
+        isin = file.split("_")[-1].split(".")[0]
+        return isin, folder
