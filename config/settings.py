692242628e0f77537d5bd2502914299e999199ee
Ali Moghimi <mghmiali@gmail.com>
2024-01-29 12:51:42 +0000

    fix: update the env variables

--- config/settings.py
+++ config/settings.py
@@ -1,12 +1,9 @@
 from decouple import config
 
-
-FILE_EXT = config("FILE_EXT", default="*")
+FILE_EXTS = config("FILE_EXTS", default="*").replace(" ", "").split(",")
 LOG_LEVEL = config("LOG_LEVEL", default="INFO")
 OUTPUT_TABLE = config("OUTPUT_TABLE")
 INSERTER_MAX_RETRIES = config("INSERTER_MAX_RETRIES", default=3, cast=int)
-REQUEST_MAX_RETRIES = config("REQUEST_MAX_RETRIES", default=3, cast=int)
-REQUEST_BACKOFF_FACTOR = config("REQUEST_BACKOFF_FACTOR", default=2, cast=int)
 MSSQL_SERVER = config("MSSQL_SERVER")
 MSSQL_DATABASE = config("MSSQL_DATABASE")
 MSSQL_USERNAME = config("MSSQL_USERNAME")
